ddd191953db7edc15d511a80baa121cfc2318c4c
Michael <3yebmb@gmail.com>
2019-04-27 09:41:20 +0500

    alternative version of simple program

--- app/src/main/java/dev/m13d/weatherapp/MainActivity.java
+++ app/src/main/java/dev/m13d/weatherapp/MainActivity.java
@@ -1,13 +1,47 @@
 package dev.m13d.weatherapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView cityName;
+    private Button toSecondActivityBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        setOnClickForToSecondActivityBtn();
+    }
+
+    private void initViews() {
+
+        toSecondActivityBtn = findViewById(R.id.toSecondActivityBtn);
+    }
+
+    private void setOnClickForToSecondActivityBtn() {
+        toSecondActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
     }
 }
---
+++ app/src/main/java/dev/m13d/weatherapp/SecondActivity.java
@@ -0,0 +1,13 @@
+package dev.m13d.weatherapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
